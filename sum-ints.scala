e6b5783bfe29e909b530448af51f1fa64221cc8c
Ricky Kwok <rickyk9487@gmail.com>
2015-06-23 15:07:48 -0700

    tail recursion, anonymous function

---
+++ sum-ints.scala
@@ -0,0 +1,8 @@
+def sum(f: Int => Int)(a: Int, b: Int): Int = {
+  def loop(a: Int, acc: Int): Int = {
+    if (a > b) acc
+    else loop(a + 1, f(a) + acc)
+  }
+  loop(a, 0)
+}
+println(sum(x => x)(1, 10))
