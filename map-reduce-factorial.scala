b53e6c02b053edff46fe23e9199761581e9357d9
Ricky Kwok <rickyk9487@gmail.com>
2015-06-23 15:25:41 -0700

    map reduce with factorial

---
+++ map-reduce-factorial.scala
@@ -0,0 +1,9 @@
+def mapReduce(f: Int => Int, combine: (Int, Int) => Int,
+                 zero: Int)(a: Int, b: Int): Int =
+    if (a > b) zero
+    else combine(f(a), mapReduce(f, combine, zero)(a + 1, b))
+
+  def product(f: Int => Int)(a: Int, b: Int): Int =
+    mapReduce(f, (x,y) => x * y, 1)(a, b)
+
+  println(product(x => x)(1, 5))
