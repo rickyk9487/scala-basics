610feaa3f3e156b08a4f0d71242db83e7006e805
Ricky Kwok <rickyk9487@gmail.com>
2015-06-23 15:04:59 -0700

    square root Newton's Method

---
+++ sqrt.scala
@@ -0,0 +1,18 @@
+def abs(x: Double) =
+  if (x <= 0) -x
+  else x
+
+def sqrtIter(guess: Double, x: Double): Double =
+  if (isGoodEnough(guess, x)) guess
+  else sqrtIter(improve(guess, x), x)
+
+def isGoodEnough(guess: Double, x: Double) =
+  abs(guess * guess - x) / x < 0.0001
+
+def improve(guess: Double, x: Double) =
+  (guess + x / guess) / 2
+
+def sqrt(x: Double) = sqrtIter(1.0, x)
+
+val y = 3.0
+println(s"The positive square root of ${y} is ${sqrt(y)}")
