d1e34375518666f1ec080c681e79d8ae64ee703d
Ricky Kwok <rickyk9487@gmail.com>
2015-07-08 16:41:40 -0700

    add polynomials, foldLeft, case

---
+++ functional-programming/week6/polynomials_add.scala
@@ -0,0 +1,24 @@
+/**
+ * Created by ricky on 7/8/15.
+ */
+object polynomials_add {
+
+  class Poly(terms0: Map[Int, Double]) {
+    def this(bindings: (Int, Double)*) = this(bindings.toMap)
+    val terms = terms0 withDefaultValue 0.0
+    def + (other: Poly) = new Poly((other.terms foldLeft terms)(addTerm))
+
+    def addTerm(terms: Map[Int, Double], term: (Int, Double)): Map[Int, Double] = {
+      val (exp, coeff) = term
+      terms + (exp -> (coeff + terms(exp)))
+    }
+    override def toString =
+      (for ((exp, coeff) <- terms.toList.sorted.reverse) yield coeff + "x^"+exp) mkString " + "
+  }
+  val p1 = new Poly(1 -> 2.0, 3 -> 4.0, 5 -> 6.2)
+  val p2 = new Poly(0 -> 3.0, 3 -> 7.0)
+  println(p1)
+  println(p2)
+  println(p1 + p2)
+  println(p1.terms(5))
+}
